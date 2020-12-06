ee10afb9dfde24565ee3c501a244be1860c2b534
Hooshmand <Techid021@gmail.com>
2020-12-06 14:23:42 -0800

    1

---
+++ KarateClub/KarateClub.Application/Services/TestService.cs
@@ -0,0 +1,27 @@
+﻿using KarateClub.Application.Interfaces;
+using KarateClub.Application.ViewModels;
+using KarateClub.Domain.Intefaces;
+using KarateClub.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KarateClub.Application.Services
+{
+    public class TestService : ITestService
+    {
+        private ITestRepository _testRepository;
+        public TestService(ITestRepository testRepository)
+        {
+            this._testRepository = testRepository;
+        }
+
+        public TestViewModel GetTests()
+        {
+            return new TestViewModel
+            {
+                Tests = _testRepository.GetTests()
+            };
+        }
+    }
+}
---
+++ KarateClub/KarateClub.Infra.Data/Repository/TestRepository.cs
@@ -0,0 +1,23 @@
+﻿using KarateClub.Domain.Intefaces;
+using KarateClub.Domain.Models;
+using KarateClub.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KarateClub.Infra.Data.Repository
+{
+    public class TestRepository : ITestRepository
+    {
+        private KarateClubDBContext _ctx;
+        public TestRepository(KarateClubDBContext ctx)
+        {
+            this._ctx = ctx;
+        }
+
+        public IEnumerable<Test> GetTests()
+        {
+            return _ctx.Tests;
+        }
+    }
+}
